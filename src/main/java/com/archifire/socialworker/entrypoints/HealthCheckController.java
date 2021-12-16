96c29fe0a7bb95a0881c24276d578e11c653311b
이승주 <kkan79@gmail.com>
2021-12-17 01:34:09 +0900

    - log4j init

---
+++ src/main/java/com/archifire/socialworker/entrypoints/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.archifire.socialworker.entrypoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping(value = "/healthcheck")
+    public String healthCheck() {
+        return "SocialWorkAPI is OK!!";
+    }
+
+
+}
--- src/main/java/com/archifire/socialworker/entrypoints/employee/EmployeeController.java
+++ src/main/java/com/archifire/socialworker/entrypoints/employee/EmployeeController.java
@@ -2,18 +2,10 @@ package com.archifire.socialworker.entrypoints.employee;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
 public class EmployeeController {
-    @GetMapping(value = "/helloworld/string")
-    public String helloworldString() {
-        log.info("test {}", "test");
-
-        return "helloworld";
-    }
 }
